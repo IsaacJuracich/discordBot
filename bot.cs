05ee3cbdcaa5adab7a6074292dbcc9a0ad83e6ee
IsaacJuracich <93289395+IsaacJuracich@users.noreply.github.com>
2021-10-29 22:21:05 -0700

    Update bot.cs
    Fixed formatting

--- bot.cs
+++ bot.cs
@@ -1,17 +1,13 @@
 // Copy and Paste for Discord Bots
 
-internal class bot
-    {
+internal class bot {
         private static CommandService _commands = new CommandService();
         private static IServiceProvider _services;
         public static DiscordSocketClient d;
         public static List<SocketMessage> collection = new List<SocketMessage>();
-        public static async Task start()
-        {
-            try
-            {
-                DiscordSocketConfig config = new DiscordSocketConfig
-                {
+        public static async Task start() {
+            try {
+                DiscordSocketConfig config = new DiscordSocketConfig {
                     MessageCacheSize = 100,
                     AlwaysDownloadUsers = true,
                 };
@@ -22,15 +18,13 @@ internal class bot
                 await d.SetGameAsync("", null, ActivityType.Playing);
                 await Task.Delay(-1);
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(e.StackTrace);
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
-        private static async Task RegisterCommands()
-        {
+        private static async Task RegisterCommands() {
             _commands = new CommandService();
             _services = new ServiceCollection()
                 .AddSingleton(d)
@@ -39,15 +33,13 @@ internal class bot
             await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
             d.MessageReceived += handler;
         }
-        private static async Task handler(SocketMessage arg)
-        {
+        private static async Task handler(SocketMessage arg) {
             collection.Add(arg);
             Console.WriteLine(arg);
             var message = arg as SocketUserMessage;
             int argPos = 0;
             var context = new SocketCommandContext(d, message);
-            if (message.HasStringPrefix("$", ref argPos) || message.HasMentionPrefix(d.CurrentUser, ref argPos))
-            {
+            if (message.HasStringPrefix("$", ref argPos) || message.HasMentionPrefix(d.CurrentUser, ref argPos)) {
                 var result = await _commands.ExecuteAsync(context, argPos, _services);
                 if (!result.IsSuccess && result.ErrorReason.ToLower() != "unknown command.")
                     Console.WriteLine(result.ErrorReason);
